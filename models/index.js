fbea94466310dcfec5d45968867b6db30d28a41d
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-07-27 11:23:24 -0500

    Delete and Pseudo code by hand for MVC of this App

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, "/../config/config.json"))[env];
 var db = {};
 
+// JawsDB Deployment
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
